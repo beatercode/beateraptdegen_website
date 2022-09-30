cafc3a4847c165fd254d4e8e5a21d9d430a8c4f0
BeaterCode <minoi.luca@gmail.com>
2022-09-30 16:59:02 +0200

    sf

--- static/js/main.7555c073.js
+++ static/js/main.7555c073.js
@@ -26036,41 +26036,7 @@
                   children: "Go to marketplace!"
                 })
               ]
-            }),
-            /*
-            (0, Ze.jsxs)("div", {
-              className: "mintingblock",
-              children: [
-                (0, Ze.jsx)("div", {
-                  className: "rampp-minting-container",
-                  children: [
-                    (0, Ze.jsx)("button", {
-
-
-
-                      className: "rampp-minting-button",
-                      "data-merkle-proof-uri":
-                        "https://us-central1-nft-rampp.cloudfunctions.net/app/FWXXucnCeyrz9SgGSVQK/merkle/verify",
-                      "data-styles":
-                        "eyJvcGVuIjp7InRleHQiOiJNaW50Iiwic3R5bGVzIjoiYm9yZGVyOm5vbmU7d2lkdGg6IDE1cmVtO3BhZGRpbmc6MC41cmVtO2ZvbnQtc2l6ZTogMS4xMjVyZW07bGluZS1oZWlnaHQ6IDEuNzVyZW07dGV4dC1hbGlnbjogY2VudGVyO2N1cnNvcjogcG9pbnRlcjtib3JkZXItcmFkaXVzOjAuMTI1cmVtO2NvbG9yOiNmZmZmZmY7YmFja2dyb3VuZC1jb2xvcjojZWY3OTViOyJ9LCJwYXVzZWQiOnsidGV4dCI6Ik1pbnRpbmclMjBub3QlMjBhdmFpbGFibGUiLCJzdHlsZXMiOiJib3JkZXI6bm9uZTt3aWR0aDogMTVyZW07cGFkZGluZzowLjVyZW07Zm9udC1zaXplOiAxLjEyNXJlbTtsaW5lLWhlaWdodDogMS43NXJlbTt0ZXh0LWFsaWduOiBjZW50ZXI7Y3Vyc29yOiBwb2ludGVyO2JvcmRlci1yYWRpdXM6MC4xMjVyZW07Y29sb3I6IzY3NjU2NTtiYWNrZ3JvdW5kLWNvbG9yOiNDQ0RCREM7Y3Vyc29yOm5vdC1hbGxvd2VkOyJ9LCJzdXBwbHlSZWFjaGVkIjp7InRleHQiOiJTb2xkJTIwb3V0Iiwic3R5bGVzIjoiYm9yZGVyOm5vbmU7d2lkdGg6IDE1cmVtO3BhZGRpbmc6MC41cmVtO2ZvbnQtc2l6ZTogMS4xMjVyZW07bGluZS1oZWlnaHQ6IDEuNzVyZW07dGV4dC1hbGlnbjogY2VudGVyO2N1cnNvcjogcG9pbnRlcjtib3JkZXItcmFkaXVzOjAuMTI1cmVtO2NvbG9yOiNlZjc5NWI7YmFja2dyb3VuZC1jb2xvcjojZWNlNGU0O2N1cnNvcjpub3QtYWxsb3dlZDsifSwiZXJyb3IiOnsidGV4dCI6Ik1pbnRpbmcgVW5hdmFpbGFibGUiLCJzdHlsZXMiOiJib3JkZXI6bm9uZTt3aWR0aDogMTVyZW07cGFkZGluZzowLjVyZW07Zm9udC1zaXplOiAxLjEyNXJlbTtsaW5lLWhlaWdodDogMS43NXJlbTt0ZXh0LWFsaWduOiBjZW50ZXI7Y3Vyc29yOiBwb2ludGVyO2JvcmRlci1yYWRpdXM6MC4xMjVyZW07Y29sb3I6I2ZmMDAwMDtiYWNrZ3JvdW5kLWNvbG9yOiNmZmI4Yjg7Y3Vyc29yOm5vdC1hbGxvd2VkOyJ9LCJjbGFpbVRleHQiOnsidGV4dCI6bnVsbCwic3R5bGVzIjoiY29sb3I6IHJnYmEoMTU2LCAxNjMsIDE3NSk7Zm9udC1zaXplOiAwLjc1cmVtO2xpbmUtaGVpZ2h0OiAxcmVtO3RleHQtYWxpZ246IGNlbnRlcjtwYWRkaW5nLXRvcDogMC4yNXJlbTtwYWRkaW5nLWJvdHRvbTogMC4yNXJlbTttYXJnaW46MDtmb250LWZhbWlseTpzYW5zLXNlcmlmOyJ9LCJxdWFudGl0eSI6eyJ0ZXh0IjpudWxsLCJzdHlsZXMiOiJ3aWR0aDo0MHB4O2NvbG9yOiNlZjc5NWI7Ym9yZGVyLXN0eWxlOnNvbGlkO2JvcmRlci13aWR0aDoxcHg7Ym9yZGVyLWNvbG9yOiNlZjc5NWI7Ym9yZGVyLXJhZGl1czowLjEyNXJlbTtmb250LXNpemU6MS4zcmVtO3RleHQtYWxpZ246Y2VudGVyOyJ9fQ==",
-                      "data-abi-link":
-                        "https://firebasestorage.googleapis.com/v0/b/nft-rampp.appspot.com/o/solidity_outputs%2FFWXXucnCeyrz9SgGSVQK%2FOninousContract_data-e2448345-bf2e-444d-ac9e-b577feaa22bc.json?alt=media",
-                      "data-redirect": "null",
-                      "data-contract-address":
-                        "0x2fb60611a511e25f577cf19a4b34a55cd53d0c40",
-                      "data-show-claim-count": "true",
-                      "data-network": "mainnet",
-                      "data-format": "multi",
-                    })
-                  ]
-                }),
-                (0, Ze.jsx)("h3", {
-                  className: "onih3",
-                  children: "Invest in Onis today!",
-                }),
-              ],
-            }),
-            */
+            })
           ],
         });
       };
